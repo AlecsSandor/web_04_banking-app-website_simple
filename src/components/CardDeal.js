de3eeea4cfdcbc1cc4592780d5bbff0b060d6b62
Alex <alex@192-168-0-100.rdsnet.ro>
2023-04-22 11:46:23 +0300

    Finished banking website - frontend

---
+++ src/components/CardDeal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { card } from '../assets'
+import Button from './Button'
+import styles, { layout } from '../style'
+
+const CardDeal = () => {
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w[470px] mt-5`}>Ilurum nexus istrem acvaticus lorem passelolodor nimuf next uistrem. Nos estre umio feersi.</p>
+        <Button styles='mt-10'/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
+  )
+}
+
+export default CardDeal
